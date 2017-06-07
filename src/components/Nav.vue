d4aa53bd9be4ccb4b181850ab2c55be542edf9fb
Adriel P. de Guzman <adrielpdeguzman@users.noreply.github.com>
2017-06-07 17:02:06 +0800

    Add BEM classes for user customization

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,16 @@
 <template>
   <nav class="Nav nav" :class="{ 'has-shadow': hasShadow }">
     <div class="container">
-      <div class="nav-left">
+      <div class="Nav__left nav-left">
         <slot name="nav-left"></slot>
       </div>
 
-      <div class="nav-center">
+      <div class="Nav__center nav-center">
         <slot name="nav-center"></slot>
       </div>
 
       <span
-        class="nav-toggle"
+        class="Nav__togle nav-toggle"
         :class="isActiveClass"
         v-if="navToggle"
         @click="isActive = !isActive"
@@ -21,7 +21,7 @@
       </span>
 
       <div
-        class="nav-right"
+        class="Nav__right nav-right"
         :class="[{ 'nav-menu': navToggle }, isActiveClass]"
       >
         <slot name="nav-right"></slot>

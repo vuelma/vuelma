33d50f25b5dd993489ecc33bd3b7df3e3e0f61c1
Adriel P. de Guzman <adrielpdeguzman@users.noreply.github.com>
2017-08-15 15:43:33 +0800

    Update Navbar usage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <navbar-component toggle transparent>
+    <navbar toggle transparent>
       <template slot="navbar-brand">
         <router-link to="/" class="navbar-item">vuelma</router-link>
       </template>
@@ -9,24 +9,19 @@
         <router-link to="/" class="navbar-item" exact>Home</router-link>
         <router-link to="/docs" class="navbar-item">Docs</router-link>
       </template>
-
-      <template slot="navbar-end">
-        <router-link to="/" class="navbar-item" exact>Home</router-link>
-        <router-link to="/docs" class="navbar-item">Docs</router-link>
-      </template>
-    </navbar-component>
+    </navbar>
 
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import NavbarComponent from '@/components/Navbar';
+import Navbar from '@/components/Navbar';
 
 export default {
   name: 'app',
   components: {
-    NavbarComponent,
+    Navbar,
   },
 };
 </script>

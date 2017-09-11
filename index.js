9429521e9a0687cb4ff39f63add555442fa10f3f
Adriel P. de Guzman <adrielpdeguzman@users.noreply.github.com>
2017-09-11 09:05:56 +0800

    Add .default to exports

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-exports.Modal = require('./src/components/Modal.vue');
-exports.Navbar = require('./src/components/Navbar.vue');
-exports.Pagination = require('./src/components/Pagination.vue');
-exports.Tabs = require('./src/components/Tabs.vue');
+exports.Modal = require('./src/components/Modal.vue').default;
+exports.Navbar = require('./src/components/Navbar.vue').default;
+exports.Pagination = require('./src/components/Pagination.vue').default;
+exports.Tabs = require('./src/components/Tabs.vue').default;
+

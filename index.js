30aa82c9da2d0036786bf3271d5232036882ca91
Adriel P. de Guzman <adrielpdeguzman@users.noreply.github.com>
2017-09-11 14:09:33 +0800

    Update NPM information
    - Re-organize dependencies

--- index.js
+++ index.js
@@ -2,4 +2,3 @@ exports.Modal = require('./src/components/Modal.vue').default;
 exports.Navbar = require('./src/components/Navbar.vue').default;
 exports.Pagination = require('./src/components/Pagination.vue').default;
 exports.Tabs = require('./src/components/Tabs.vue').default;
-

f6090619a7821d4dd966c95d82ac97b42f2cbd1a
Adriel P. de Guzman <adrielpdeguzman@users.noreply.github.com>
2017-09-18 21:09:18 +0800

    Add missing exports to package

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+exports.Dropdown = require('./src/components/Dropdown.vue').default;
 exports.Modal = require('./src/components/Modal.vue').default;
 exports.Navbar = require('./src/components/Navbar.vue').default;
+exports.Notification = require('./src/components/Notification.vue').default;
 exports.Pagination = require('./src/components/Pagination.vue').default;
 exports.Tabs = require('./src/components/Tabs.vue').default;

ff3a4d0b2e2de2dfc157c12b74a8e95b2e7912b3
Adriel P. de Guzman <adrielpdeguzman@users.noreply.github.com>
2017-09-19 00:32:29 +0800

    Update packaging and export behavior
    - Now compiles the package before publish
    - Only publish the compiled files (/dist)

--- index.js
+++
@@ -1,6 +0,0 @@
-exports.Dropdown = require('./src/components/Dropdown.vue').default;
-exports.Modal = require('./src/components/Modal.vue').default;
-exports.Navbar = require('./src/components/Navbar.vue').default;
-exports.Notification = require('./src/components/Notification.vue').default;
-exports.Pagination = require('./src/components/Pagination.vue').default;
-exports.Tabs = require('./src/components/Tabs.vue').default;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import Breadcrumb from './components/Breadcrumb';
+import Dropdown from './components/Dropdown';
+import Menu from './components/Menu';
+import Modal from './components/Modal';
+import Navbar from './components/Navbar';
+import Notification from './components/Notification';
+import Pagination from './components/Pagination';
+import Tabs from './components/Tabs';
+
+export {
+  Breadcrumb,
+  Dropdown,
+  Menu,
+  Modal,
+  Navbar,
+  Notification,
+  Pagination,
+  Tabs,
+};

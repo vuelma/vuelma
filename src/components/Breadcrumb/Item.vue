950feb6a28ac3b06aa58c44c2141ffbea14a461d
Isaiah Cruz <isaiahgcruz@gmail.com>
2017-09-18 23:57:04 +0800

    Update breadcrumb component
    - create breadcrumb-item component
    - add a few comments

---
+++ src/components/Breadcrumb/Item.vue
@@ -0,0 +1,80 @@
+<template>
+  <li :class="{ 'is-active': isActive }">
+    <a @click="onItemClick">
+      <icon
+        v-if="hasIcon"
+        :name="iconName"
+        v-bind="iconModifiers"
+      ></icon>
+      <span v-html="label"></span>
+    </a>
+  </li>
+</template>
+
+<script>
+import modifiers from '../../utils/modifiers';
+import Icon from '../Icon';
+
+const iconModifiers = [
+  ...modifiers.sizes,
+];
+
+export default {
+  name: 'breadcrumb-item',
+  components: {
+    Icon,
+  },
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+    },
+    icon: [String, Object],
+    isActive: Boolean,
+  },
+  computed: {
+    hasIcon() {
+      return (
+        typeof this.icon === 'string'
+        || (
+          typeof this.icon === 'object'
+          && this.icon.name
+        )
+      );
+    },
+
+    iconName() {
+      if (this.hasIcon) {
+        if (typeof this.icon === 'object' && this.icon.name) {
+          return this.icon.name;
+        }
+
+        return this.icon;
+      }
+
+      return '';
+    },
+
+    iconModifiers() {
+      if (typeof this.icon === 'object') {
+        return {
+          ...modifiers.generate(iconModifiers, this.icon),
+        };
+      }
+
+      return {
+        'is-small': true,
+      };
+    },
+  },
+  methods: {
+    onItemClick() {
+      this.$emit('click:item', this.$props);
+    },
+  },
+};
+</script>
+

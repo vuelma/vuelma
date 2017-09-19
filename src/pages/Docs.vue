931d8f49a0dd2c8f7fe7bed773e34583721b03ff
Adriel P. de Guzman <adrielpdeguzman@users.noreply.github.com>
2017-09-19 10:07:09 +0800

    Add usage samples on Docs page

--- src/pages/Docs.vue
+++ src/pages/Docs.vue
@@ -21,16 +21,51 @@
         My Custom Dropdown
       </template>
     </dropdown>
+
+    <h1 class="title is-1">Notification</h1>
+    <button
+      type="button"
+      class="button is-primary"
+      @click="showNotification = true">
+      Show Notification
+    </button>
+    <notification
+      auto-close
+      is-warning
+      :show.sync="showNotification"
+    >
+      Sample Notification
+    </notification>
+
+    <h1 class="title is-1">Pagination</h1>
+    <pagination
+      is-medium
+      is-centered
+      :current-page.sync="currentPage"
+      :page-size="10"
+      :total-items="1000"
+      :padding="3"
+    ></pagination>
   </div>
 </template>
 
 <script>
 import Dropdown from '@/components/Dropdown';
+import Notification from '@/components/Notification';
+import Pagination from '@/components/Pagination';
 
 export default {
   name: 'docs',
   components: {
     Dropdown,
+    Notification,
+    Pagination,
+  },
+  data() {
+    return {
+      showNotification: false,
+      currentPage: 1,
+    };
   },
 };
 </script>
